253b050f25d57a42e9a4125c988ddd031328cb09
aivazashvilitekla <aivazashvilitekla7@gmail.com>
2021-05-26 13:52:51 +0400

    added locations

--- OurWebApplication/OurWebApplication/Controllers/ValuesController.cs
+++ OurWebApplication/OurWebApplication/Controllers/ValuesController.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using OurWebApplication.EF;
+using OurWebApplication.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Net;
@@ -10,30 +12,81 @@ namespace OurWebApplication.Controllers
     public class ValuesController : ApiController
     {
         // GET api/values
-        public IEnumerable<string> Get()
+        public IEnumerable<LocationViewModel> Get()
         {
-            return new string[] { "value1", "value2" };
+            using (LearningCenterContext db = new LearningCenterContext())
+            {
+                return db.Locations.Select(i => new LocationViewModel
+                {
+                    Name = i.Name,
+                    Latitude = i.Latitude,
+                    Longtitude = i.Longtitude
+                }).ToList();
+            }
         }
 
         // GET api/values/5
-        public string Get(int id)
+        public LocationViewModel Get(string nm)
         {
-            return "value";
+            using (LearningCenterContext db = new LearningCenterContext())
+            {
+                return db.Locations.Where(i => i.Name.Equals(nm)).Select(i => new LocationViewModel
+                {
+                    Name = i.Name,
+                    Longtitude = i.Longtitude,
+                    Latitude = i.Latitude
+                }).FirstOrDefault();
+            }
         }
 
         // POST api/values
-        public void Post([FromBody]string value)
+        public void Post([FromBody] LocationViewModel l)
         {
+            using (LearningCenterContext db = new LearningCenterContext())
+            {
+                Location location = new Location
+                {
+                    Name = l.Name,
+                    Longtitude = l.Longtitude,
+                    Latitude = l.Latitude
+                };
+                db.Locations.Add(location);
+                db.SaveChanges();
+            }
         }
 
         // PUT api/values/5
-        public void Put(int id, [FromBody]string value)
+        public void Put(string nm, [FromBody] LocationViewModel l)
         {
+            using (LearningCenterContext db = new LearningCenterContext())
+            {
+                if (!db.Locations.Any(i => i.Name.Equals(nm)))
+                    throw new Exception($"ადგილმდებარეობა სახელით {nm} ვერ მოიძებნა!");
+                var r = db.Locations.Where(i => i.Name.Equals(nm)).First();
+
+                r.Name = l.Name;
+                r.Longtitude = l.Longtitude;
+                r.Latitude = l.Latitude;
+                r.ParentID = l.ParentID;
+
+                db.SaveChanges();
+            }
         }
 
         // DELETE api/values/5
-        public void Delete(int id)
+        public void Delete(string nm)
         {
+            using (LearningCenterContext db = new LearningCenterContext())
+            {
+                if (!db.Locations.Any(i => i.Name.Equals(nm)))
+                    throw new Exception($"ადგილმდებარეობა სახელით {nm} ვერ მოიძებნა!");
+
+
+                var r = db.Locations.Where(i => i.Name.Equals(nm)).First();
+
+                db.Locations.Remove(r);
+                db.SaveChanges();
+            }
         }
     }
 }
---
+++ OurWebApplication/OurWebApplication/Models/LocationViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace OurWebApplication.Models
+{
+    public class Class1
+    {
+        public string Name { get; set; }
+
+        public double Longtitude { get; set; }
+
+        public double Latitude { get; set; }
+        public int? ParentID { get; set; }
+    }
+}
